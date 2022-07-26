91de3ab7f37efa3398aab418567ec44a5710066b
kvbwork <kvbdev@yandex.ru>
2022-07-26 15:54:14 +0500

    Launcher.main() arguments forwarding

--- src/kvbdev/Launcher.java
+++ src/kvbdev/Launcher.java
@@ -9,13 +9,13 @@ public class Launcher {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Задача 1: Установка");
-        InstallMain.main(null);
+        InstallMain.main(args);
 
         System.out.println("Задача 2: Сохранение");
-        SavingMain.main(null);
+        SavingMain.main(args);
 
         System.out.println("Задача 3: Загрузка");
-        LoadingMain.main(null);
+        LoadingMain.main(args);
     }
 
 }
